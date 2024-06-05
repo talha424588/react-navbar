334add217abd0efcbd11d7ad0565016ae7a5753b
Muhammad Talha <talha424588@gmail.com>
2024-06-05 08:28:59 +0500

    navbar structure added

---
+++ src/componenets/Navbar.jsx
@@ -0,0 +1,31 @@
+
+import './Navbar.css';
+import logo_light from '../assets/logo-white.png'
+import logo_black from '../assets/logo-black.png'
+import search_icon_light from '../assets/search-w.png' 
+import search_icon_black from '../assets/search-b.png' 
+import toggle_icon_light from '../assets/day.png' 
+import toggle_icon_black from '../assets/night.png' 
+function Navbar()
+{
+    return (
+    <>
+        <div className="navbar">
+            <img src={logo_black} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Feature</li>
+                <li>About</li>
+            </ul>
+
+            <div className="search-box">
+                <input type="text" placeholder="Search" />
+                <img src={search_icon_black} alt="" srcset="" />
+            </div>
+            <img src={toggle_icon_black} alt="toggle-button" />
+        </div>
+    </>
+    )
+}
+export default Navbar;
